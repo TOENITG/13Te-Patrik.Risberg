305010efadbcbca7b597377a6b7c28d08e949699
Tobias Englund <tobias.englund@it-gymnasiet.se>
2014-10-16 15:04:19 +0200

    Kommentar
    Kommentar igen

---
+++ Inlämningsuppgift/src/StringVars/Uppgift1.java
@@ -0,0 +1,40 @@
+package StringVars;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Patrik
+ */
+
+public class Uppgift1 {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        
+        String first_name;
+        String family_name;
+        String civic_number;
+        String address;
+        
+        System.out.println("Skriv ditt förnamn.");
+        first_name=user_input.next();
+        
+        System.out.println("Skriv ditt efternamn.");
+        family_name=user_input.next();
+        
+        System.out.println("Skriv ditt personnummer.");
+        civic_number=user_input.next();
+        
+        System.out.println("Skriv din adress.");
+        address=user_input.next();
+        
+        System.out.println("Välkommen " + first_name + ", då du fyller den " + civic_number.substring(2, 6) + " kommer vi komma till dig, " + address + " och fira dig."
+    }
+}
+
+/*
+Tack!
+
+Snyggt och enkelt och lätt att följa. Det saknas kommentarer!
+
+*/
---
+++ Inlämningsuppgift/src/StringVars/Uppgift2.java
@@ -0,0 +1,46 @@
+package StringVars;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Patrik
+ */
+public class StringVars {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        
+        String first;
+        String second;
+        String third;
+        String fourth;
+        String fifth;
+        /**
+         * String på alla variabler innan koden började
+         * för att lättare hålla koll
+         */
+    
+        System.out.println("Skriv ett ord i taget. Första ordet");
+        first=user_input.next();
+        /**
+         * Koden lägger på ord för ord och härmar.
+         * Koden lägger på ett mellanrum mellan varje ord.
+         */
+        System.out.println(first);
+        second=user_input.next();
+        
+        System.out.println(first + " " + second);
+        third=user_input.next();
+        
+        System.out.println(first + " " + second + " " + third);
+        fourth=user_input.next();
+        
+        System.out.println(first + " " + second + " " + third + " " + fourth);
+        fifth=user_input.next();
+        
+        System.out.println(first + " " + second + " " + third + " " + fourth + " " + fifth);
+        /**
+         * Programmet är slut efter att ord fem upprepats.
+         */
+    }
+}
